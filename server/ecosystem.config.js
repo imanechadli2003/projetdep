230aee44cc5182896799c433afedb1bdc3fae3dc
imanechadli2003 <imanechadli20003@gmail.com>
2025-02-10 18:46:51 +0100

    voilaa

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,14 +1,1 @@
-module.export={
-    apps:[
-        {
-            name:"projetdep",
-            script:"npm",
-            args:"run dev",
-            env:{
-                NODE_ENV:"development",
-                ENV_VAR1:"enviroment variable",
-            }
-
-        }
-    ]
-}
+module.exports = { apps : [{ name: 'projetdep', script: 'npm', args: 'run dev', env: { NODE_ENV: 'development', ENV_VAR1: 'environment-variable', } }], };
